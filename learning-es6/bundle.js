1fd0cfcf17a7760b590e57c455ac47656c55b2c6
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2018-11-21 17:33:04 -0300

    Feature de classes

--- learning-es6/bundle.js
+++ learning-es6/bundle.js
@@ -1,5 +1,17 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -8,20 +20,79 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 alert('Teste');
 
-var Test =
+var List =
 /*#__PURE__*/
 function () {
-  function Test() {
-    _classCallCheck(this, Test);
+  function List() {
+    _classCallCheck(this, List);
+
+    this.data = [];
   }
 
-  _createClass(Test, [{
-    key: "metodo",
-    value: function metodo() {}
-  }, {
-    key: "outro",
-    value: function outro() {}
+  _createClass(List, [{
+    key: "add",
+    value: function add(data) {
+      this.data.push(data);
+      console.log(this.data);
+    }
   }]);
 
-  return Test;
+  return List;
 }();
+
+var TodoList =
+/*#__PURE__*/
+function (_List) {
+  _inherits(TodoList, _List);
+
+  function TodoList() {
+    var _this;
+
+    _classCallCheck(this, TodoList);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(TodoList).call(this));
+    _this.usuario = "Thiago";
+    return _this;
+  }
+
+  _createClass(TodoList, [{
+    key: "mostraUsuario",
+    value: function mostraUsuario() {
+      console.log(this.usuario);
+    }
+  }]);
+
+  return TodoList;
+}(List);
+
+var Matematica =
+/*#__PURE__*/
+function () {
+  function Matematica() {
+    _classCallCheck(this, Matematica);
+  }
+
+  _createClass(Matematica, null, [{
+    key: "soma",
+    value: function soma(a, b) {
+      return a + b;
+    }
+  }]);
+
+  return Matematica;
+}();
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+  MinhaLista.add('Novo todo');
+};
+
+document.getElementById('usuario').onclick = function () {
+  MinhaLista.mostraUsuario();
+};
+
+document.getElementById('soma').onclick = function () {
+  var valor = Matematica.soma(10, 40);
+  console.log(valor);
+};
--- learning-es6/main.js
+++ learning-es6/main.js
@@ -1,7 +1,43 @@
 alert('Teste');
 
-class Test {
-    metodo(){}
+class List {
+    constructor(){
+        this.data = [];
+    }
 
-    outro(){}
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+
+    }
+}
+
+class TodoList extends List{
+    constructor(){
+        super();
+        this.usuario = "Thiago";
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+class Matematica { 
+    static soma(a, b){
+        return a + b;
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.add('Novo todo');
+}
+document.getElementById('usuario').onclick = function () {
+    MinhaLista.mostraUsuario();
+}
+document.getElementById('soma').onclick = function () {
+    const valor = Matematica.soma(10, 40);
+    console.log(valor);
 }
